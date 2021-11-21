2c763bdb9190df80eddbe7aac6bd69d2788e3c52
Евгений <volat-asilak@mail.ru>
2021-11-21 15:12:32 +0300

    version 1

---
+++ pick_a_card.rb
@@ -0,0 +1,5 @@
+
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'V', 'D', 'K', 'A']
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Бубны, Черви, Крести, Пики
+
+puts "#{values.sample} of #{suits.sample}"
