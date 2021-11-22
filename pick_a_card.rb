5dd60880d8a82e0b52a1ada2acd4e5aa909da1b7
Евгений <volat-asilak@mail.ru>
2021-11-23 01:38:33 +0300

    version23.11.21

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,7 +1,17 @@
 
-# encoding UTF-8
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'V', 'D', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Бубны, Черви, Крести, Пики
+# XXX/ Этот код необходим только при использовании русских букв на Windows
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
 
-puts "#{values.sample} of #{suits.sample}"
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+# /XXX
+#
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Валет', 'Дама', 'Король', 'Туз']
+suits = ['Бубны', 'Черви', 'Крести', 'Пики'] # Бубны, Черви, Крести, Пики
+
+puts "#{values.sample} #{suits.sample}"
 
